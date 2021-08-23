51522f3b5823b24cf284f027f024e3a1216a3fa3
Sol C <solcataldo98@gmail.com>
2021-08-23 11:28:05 -0300

    Agrego archivos de repaso

---
+++ strings/st4.c
@@ -0,0 +1,18 @@
+
+/*Leer 5 strings por teclado y almacenarlos en un archivo.*/
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+	char palabra[20]; 
+	int i; 
+
+	for (i = 0; i <5; i++)
+	printf("Ingrese una palabra: "); 
+	scanf("%c", )
+
+
+
+}
