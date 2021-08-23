08540939ee91e1ebf9a7e2892af0d72b172f3ecb
Sol C <solcataldo98@gmail.com>
2021-08-23 13:16:16 -0300

    Resolución de ejercicio 4

--- strings/st4.c
+++ strings/st4.c
@@ -2,17 +2,31 @@
 /*Leer 5 strings por teclado y almacenarlos en un archivo.*/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
 int main(){
+	int i, n; 
 	char palabra[20]; 
-	int i; 
+	
+	FILE *arch; 
 
-	for (i = 0; i <5; i++)
-	printf("Ingrese una palabra: "); 
-	scanf("%c", )
+	arch = fopen("palabras.dat", "wb"); 
 
+	if (arch != NULL){
+		for(i = 0; i < 5; i++){
 
+			printf("Ingrese una palabra: "); 
+			scanf("%s", palabra); 
+
+			fprintf(arch, "%s", palabra);
+		}
+	} else {
+		printf("Error al abrir el archivo para escritura \n"); 
+		exit(1); 
+	}
 
 }
+
+
