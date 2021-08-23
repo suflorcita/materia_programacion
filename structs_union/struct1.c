d672166c79bccee38c390a691e7d5e08e57050ec
Sol C <solcataldo98@gmail.com>
2021-08-23 19:09:24 -0300

    Resolución problema 1

---
+++ structs_union/struct1.c
@@ -0,0 +1,52 @@
+/*Leer por teclado los detalles (legajo, nombre, promedio) de n estudiantes (n <
+100) y almacenarlo en el archivo estudiantes.dat.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#define MAX 100
+
+/*Defino un struct con los datos del estudiante*/
+struct estudiante{
+	int legajo; 
+	char nombre[100]; 
+	float promedio;
+
+}; 
+
+int main(){
+	int leg, n, i = 0; 
+	struct estudiante ve; // declaro vector de estudiantes
+
+	FILE *arch; 
+	
+
+	arch = fopen("estudiantes.dat", "wb"); //abro el archivo 
+
+	if (arch == NULL){
+		printf("Error al abrir el archivo para escritura \n"); 
+		exit(1); 
+	}
+
+	printf("Ingrese el número de legajo, si ingresa 0 termina el programa: "); 
+	scanf("%d", &leg); 
+
+	while (leg != 0){
+		ve.legajo = leg; 
+
+		printf("Ingrese el nombre del alumno: "); 
+		scanf("%s", ve.nombre); 
+
+		printf("Ingrese el promedio del estudiante: "); 
+		scanf("%f", &ve.promedio); 
+
+		fwrite(&ve,sizeof(ve),1,arch); //Escribo en el archivo la información de cada estudiante 
+		
+		printf("Ingrese el número de legajo, si ingresa 0 termina el programa: "); 
+		scanf("%d", &leg); 
+
+	}
+
+	fclose(arch); //ciero el archivo
+	return 0; 
+}
