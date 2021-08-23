a8f78f51281e93085f57c48bcddf138a3ae94693
Sol C <solcataldo98@gmail.com>
2021-08-23 10:28:23 -0300

    Resolución ejercicio 3

--- strings/st3.c
+++ strings/st3.c
@@ -3,28 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
-
-
 int main(){
 
-	char palabra[20]; 
-	int tam = 0, i;
+	char palabra[20], mayor[20]; 
+	int comp, i;
 
 
-	for(i = 0; i < 5; i++){
-		printf("Ingrese una palabra: "); 
-		scanf("%c", palabra); 
-
-		tam = strlen(palabra) 
+	printf("Ingrese una palabra: "); 
+	scanf("%s", mayor); 
 
 
+	for(i = 0; i < 4; i++){
+		printf("Ingrese otra palabra: ");
+		scanf("%s", palabra); 
 
+		comp = strcmp(palabra, mayor);
+
+		if(comp > 0) strcpy(mayor, palabra); 
 	}
 
+	printf("%s", mayor); 
 
-
-
-
-
+	return 0; 
 
 }
