d9e185f1ed69487ee6903a7bd822a5c8d95988b9
Sol C <solcataldo98@gmail.com>
2021-09-19 00:36:27 -0300

    Clase de memoria dinámica

---
+++ memoria_dinamica/ejemplos_clase/ejemplo_malloc.c
@@ -0,0 +1,25 @@
+/*
+Ejemplo simple de uso de memoria dinamica
+Programacion UNSAM
+Autor: David Lopez
+Año: 2020
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	int *ptr;
+
+	//Reservo memoria para un int
+    ptr = (int*) malloc (sizeof (int));
+
+	if (ptr != NULL) { //Verifico si la pudo reservar
+		*ptr = 10; //Almaceno un valor en la memoria reservada
+		printf ("ptr recibio el valor (direccion) %p \n", ptr);
+		printf ("El contenido de la direccion es %d \n", *ptr);
+		free (ptr); //Libero la memoria
+		ptr = NULL; //Buena practica poner en NULL el puntero liberado
+	}
+    return 0;
+}
